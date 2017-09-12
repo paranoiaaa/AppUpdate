db946bd6cbaf52d01ba02cfcbe973d3949c3cd58
Paranoia <wqd_1994@163.com>
2017-09-12 16:06:02 +0800

    add update demo

--- app/src/main/java/com/wqd/app/appupdate/MainActivity.java
+++ app/src/main/java/com/wqd/app/appupdate/MainActivity.java
@@ -2,14 +2,17 @@ package com.wqd.app.appupdate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.app.update.AppUpdate;
 import com.app.update.AppUpdateCheckUpdate;
 import com.app.update.DownloadServiceConnection;
+import com.wqd.app.dialog.ShowAlertDialog;
+import com.wqd.app.listener.DialogOnClickListener;
 
 public class MainActivity extends AppCompatActivity {
-    private boolean isUpdate;
-    private String url;
+    private boolean isUpdate=true;
+    private String url ="http://shouji.360tpcdn.com/170831/0577eae6fcaf3744efb07aee53272f18/com.shuchuang.shihua_57.apk";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +22,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void checkUpdate() {
                 if (isUpdate){
-                    AppUpdate.update(MainActivity.this,url);
+                    new ShowAlertDialog.Builder(MainActivity.this).setTitleText("Update").setContentText("Update Context").setLeftButtonText("Cancel").setRightButtonText("Confirm")
+                            .setCanceledOnTouchOutside(false).setOnclickListener(new DialogOnClickListener() {
+                        @Override
+                        public void clickLeftButton(View view) {
+
+                        }
+
+                        @Override
+                        public void clickRightButton(View view) {
+                            AppUpdate.update(MainActivity.this,url);
+                        }
+                    }).build().show();
                 }
             }
         });
